069ef22b4b6866155dceb1ecde0a04880b6a18f0
Eda Dener <81362322+edadener@users.noreply.github.com>
2021-06-22 17:50:09 +0300

    projects

--- Metotlar/DeseneGoreMetot/src/main.java
+++ Metotlar/DeseneGoreMetot/src/main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
-
 public class main {
- 
-            public static void main(String[] args) {
+    public static void metot(int number, int tmp, boolean status) {
 
+        if (number <= tmp) {
+            System.out.println(number);
+            if (number > 0 && !status) {
+                metot(number-5,tmp,false);
+            }else{
+                metot(number+5,tmp,true);
             }
+        }
     }
 
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz: ");
+        int n = inp.nextInt();
+        int tmp = n;
+        metot(n,tmp,false);
+
+    }
+
+}
